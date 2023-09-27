59552ad801fce6002fd15d008ecd7020254738d6
Baddrummer01 <143623627+Baddrummer01@users.noreply.github.com>
2023-09-26 22:18:17 -0400

    Add files via upload
    Adding main files to the program.

---
+++ minHeap.cpp
@@ -0,0 +1,68 @@
+//Name: Bennett Starks
+//Date Submitted: 04/16/2023
+//Lab Section: 002
+//Assignment Name: Min-Heap Implementation Using a Vector 
+
+#include <vector>
+#include "minHeap.h"
+
+using namespace std;
+
+void minHeap::siftUp(int pos) {
+  while (pos > 0 && heap[pos] < heap[(pos-1)/2]) {
+    swap(heap[pos], heap[(pos-1)/2]);
+    pos = (pos-1)/2;
+  }
+}
+
+void minHeap::siftDown(int pos) {
+  int smallest = pos;
+  int left = 2 * pos + 1;
+  int right = 2 * pos + 2;
+
+  while (left < heap.size()) {
+    //if the left branch of the tree is smaller than the current node, move left
+    if (heap[left] < heap[smallest]) {
+      smallest = left;
+    }
+
+    //if the right branch of the tree is smaller than the current node, move right
+    if (right < heap.size() && heap[right] < heap[smallest]) {
+      smallest = right;
+    }
+
+    //if neither of the precious conditions happened, break the loop
+    if (smallest == pos) {
+      break;
+    }
+
+    swap(heap[pos], heap[smallest]);
+    pos = smallest;
+    left = 2 * pos + 1;
+    right = 2 * pos + 2;
+  }
+}
+
+//Should build the heap from bottom-up
+minHeap::minHeap(vector<int> data) {
+  heap = data;
+  for (int i = (heap.size() - 2)/2; i >= 0; i--) {
+    siftDown(i);
+  }
+}
+
+//Insert value into heap
+void minHeap::insert(int value) {
+  heap.push_back(value);
+  siftUp(heap.size()-1);
+}
+
+//Removes minimum value
+int minHeap::removeMin() {
+  int minValue = heap[0];
+  //puts the back value at the front then sifts it down
+  heap[0] = heap.back();
+  heap.pop_back();
+  siftDown(0);
+  return minValue;
+}
---
+++ minHeap.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <vector>
+
+using namespace std;
+
+//This class defines a heap where each node has a
+//less or equal value than all child nodes (min-heap)
+//Implemented using a vector of integers representing the
+//values at each node of a complete binary tree
+//Do not modify anything in the class definition
+
+class minHeap
+{
+    private:
+        vector<int> heap;
+        //Implement the following functions that maintain the heap property
+        void siftUp(int pos);
+
+        void siftDown(int pos);
+
+    public:
+        minHeap() {} //Default constructor, heap is initiallay an empty vector
+        vector<int> getHeap() {return heap;} //Returns heap data for test cases
+        //Implement all the following functions
+
+        //Should build the heap from bottom-up
+        minHeap(vector<int> data);
+        //Insert value into heap
+        void insert(int value);
+        //Removes minimum value
+        int removeMin();
+};
